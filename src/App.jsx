ae484f84b80df1ae67ba03ef53d9afedb409251c
Thirumalaivasan Perumal <dhanushholla.mdh@gmail.com>
2024-10-16 15:22:33 +0530

    vercel analytics deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,12 @@
 import './App.css'
 import Panel from './components/Panel'
-
+import { Analytics } from "@vercel/analytics/react"
 function App() {
 
   return (
     <>
       <Panel/>
+      <Analytics/>
     </>
   )
 }
